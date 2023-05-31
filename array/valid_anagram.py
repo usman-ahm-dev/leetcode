7e34eba5927f88ce7e43f695aba7314ed8b2a4f5
Usman <usman_ahm@icloud.com>
2023-05-31 10:11:55 +0500

    Solved Problem 242

--- array/valid_anagram.py
+++ array/valid_anagram.py
@@ -7,5 +7,28 @@ phrase, typically using all the original letters exactly once.
 
 
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        pass
+    def is_anagram(self, s: str, t: str) -> bool:
+        """Given two strings s and t, returns true if t is an anagram of s, and false
+        otherwise.
+
+        Args:
+            s (str):
+            t (str):
+
+        Returns:
+            bool:
+        """
+        if len(s) != len(t):
+            return False
+
+        s_dict, t_dict = {}, {}
+
+        for i in range(len(s)):
+            s_dict[s[i]] = 1 + s_dict.get(s[i], 0)
+            t_dict[t[i]] = 1 + t_dict.get(t[i], 0)
+
+        for key in s_dict:
+            if s_dict[key] != t_dict.get(key, 0):
+                return False
+
+        return True
