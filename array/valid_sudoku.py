a771942008afb389162b1346fd0f7e35e038a4fb
Usman <usman_ahm@icloud.com>
2023-06-01 10:22:06 +0500

    Solved Problem 36

---
+++ array/valid_sudoku.py
@@ -0,0 +1,57 @@
+"""36. Valid Sudoku
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated
+according to the following rules:
+
+    Each row must contain the digits 1-9 without repetition.
+    Each column must contain the digits 1-9 without repetition.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without
+    repetition.
+
+Note:
+    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+    Only the filled cells need to be validated according to the mentioned rules.
+"""
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def is_valid_sudoku(self, board: list[list[str]]) -> bool:
+        """Determines if a 9 x 9 Sudoku board is valid.
+
+        Args:
+            board (list[list[str]]): A 9x9 List of Strings
+                Example:
+                board =
+                [["5","3",".",".","7",".",".",".","."]
+                ,["6",".",".","1","9","5",".",".","."]
+                ,[".","9","8",".",".",".",".","6","."]
+                ,["8",".",".",".","6",".",".",".","3"]
+                ,["4",".",".","8",".","3",".",".","1"]
+                ,["7",".",".",".","2",".",".",".","6"]
+                ,[".","6",".",".",".",".","2","8","."]
+                ,[".",".",".","4","1","9",".",".","5"]
+                ,[".",".",".",".","8",".",".","7","9"]]
+
+        Returns:
+            bool:
+        """
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == ".":
+                    continue
+                if (
+                    board[row][col] in rows[row]
+                    or board[row][col] in cols[col]
+                    or board[row][col] in squares[(row // 3, col // 3)]
+                ):
+                    return False
+                cols[col].add(board[row][col])
+                rows[row].add(board[row][col])
+                squares[(row // 3, col // 3)].add(board[row][col])
+        return True
