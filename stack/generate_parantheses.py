ee3b619ae6e3cfbd60614c53cff48504be044409
Usman <usman_ahm@icloud.com>
2023-06-02 15:44:42 +0500

    Solved Problem 22

---
+++ stack/generate_parantheses.py
@@ -0,0 +1,48 @@
+"""22. Generate Parentheses
+Given n pairs of parentheses, write a function to generate all combinations of
+well-formed parentheses.
+"""
+
+
+class Solution:
+    def generate_parentheses(self, n: int) -> list[str]:
+        """Given n, generates all combinations of
+        well-formed parentheses.
+
+        Args:
+            n (int):
+
+        Returns:
+            list[str]:
+        """
+        stack = []
+        result = []
+
+        def backtracking(opened, closed):
+            """Recursive function to generate all combinations of
+            well-formed parentheses.
+
+            Args:
+                opened (int): Number of ( paranthesis
+                closed (int): Number of ) paranthesis
+            """
+            if opened == closed == n:
+                result.append("".join(stack))
+                return
+
+            if opened < n:
+                stack.append("(")
+                backtracking(opened + 1, closed)
+                stack.pop()
+
+            if closed < opened:
+                stack.append(")")
+                backtracking(opened, closed + 1)
+                stack.pop()
+
+        backtracking(0, 0)
+        return result
+
+
+obj = Solution()
+obj.generate_parenthesis(3)
