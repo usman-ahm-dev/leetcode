82acff228b16eb0a4e6013ba3c469574de32c70f
Usman <usman_ahm@icloud.com>
2023-06-02 13:15:33 +0500

    Solved Problem 155

---
+++ stack/min_stack.py
@@ -0,0 +1,54 @@
+"""155. Min Stack
+Design a stack that supports push, pop, top, and retrieving the minimum element in
+constant time.
+
+Implement the MinStack class:
+
+    MinStack() initializes the stack object.
+    void push(int val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+
+You must implement a solution with O(1) time complexity for each function.
+"""
+
+
+class MinStack:
+    def __init__(self):
+        """Initializes the stack object."""
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        """Pushes the element val onto the stack.
+
+        Args:
+            val (int):
+        """
+        self.stack.append(val)
+        if self.min_stack:
+            self.min_stack.append(min(val, self.min_stack[-1]))
+        else:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        """Removes the element at the top of the stack."""
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        """Gets the top element of the stack.
+
+        Returns:
+            int:
+        """
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        """Retrieves the minimum element in the stack.
+
+        Returns:
+            int:
+        """
+        return self.min_stack[-1]
