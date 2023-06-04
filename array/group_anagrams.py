f610c096603f27e1e93319383984dc12ab4bd295
Usman <usman_ahm@icloud.com>
2023-06-04 11:46:20 +0500

    Solved Problem 49

--- array/group_anagrams.py
+++ array/group_anagrams.py
@@ -19,12 +19,31 @@ class Solution:
         Returns:
             list[list[str]]: List of grouped anagrams (List of strings)
         """
-        anagram_dict = defaultdict(list)
 
-        for s in strs:
-            char_key = [0] * 26
-            for char in s:
-                char_key[ord(char) - ord("a")] += 1
-            anagram_dict[tuple(char_key)].append(s)
+        """Create N lists that each store character frequency per string
+        (e.g [0, 0, 1, 3, 0, 1] = "cdddf"). Treat each list as a key and insert
+        strings accordingly.
 
-        return anagram_dict.values()
+        METHOD 1: T = O(N) S = O(N) 
+        (a) Create N lists of size 26 by iterating over N strings.
+        (b) Iterate over each string, and +1 count at the proper index in the list for
+        each character.
+            (ba) Index for each character is decided by subtracting ASCII of char from
+            ASCII of "a" by ord() function (e.g A = 0, B = 1, C = 2 etc.).
+        (c) Insert into dictionary with the list (converted to tuple because of hash
+        issues) as key and the string as value.
+
+        METHOD 2: T = O(MNLogN) S = O(N)
+        (a) Sort all strings.
+        (b) Iterate and insert same strings in the same list of strings.
+        """
+
+        # METHOD 1
+        result = defaultdict(list)
+        for string in strs:
+            key = [0] * 26
+            for char in string:
+                key[ord(char) - ord("a")] += 1
+            result[tuple(key)].append(string)
+
+        return result.values()
