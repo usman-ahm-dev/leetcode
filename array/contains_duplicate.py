5afb69769d611aecef19cdaec72af090144d3b24
Usman <usman_ahm@icloud.com>
2023-06-04 07:52:25 +0500

    Solved Problem 217

--- array/contains_duplicate.py
+++ array/contains_duplicate.py
@@ -15,10 +15,4 @@ class Solution:
         Returns:
             bool:
         """
-        nums_set = set()
-        for num in nums:
-            if num in nums_set:
-                return True
-            else:
-                nums_set.add(num)
-        return False
+        return True if len(nums) != len(set(nums)) else False
