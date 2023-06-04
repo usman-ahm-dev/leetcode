d0d13473cb3456fdf80b38cb7691539c4f7fb8c2
Usman <usman_ahm@icloud.com>
2023-06-04 15:30:53 +0500

    Solved Problem 347

--- array/top_K_frequent_elements.py
+++ array/top_K_frequent_elements.py
@@ -18,18 +18,36 @@ class Solution:
         Returns:
             list[int]:
         """
-        freq_count = defaultdict(int)
-        freq_list = [[] for i in range(len(nums) + 1)]  # Index represents frequency
+
+        """
+        Create a list of lists where each index represents a frequency and
+        elements at that position occur index number of times. List of lists because
+        multiple elements can occur the same number of times.
+
+        METHOD 1: T = O(N) S = O(N)
+        (a) Create a frequency map for counting and list of size len(nums) +1 for doing
+        the aforementioned thing.
+        (b) Count frequencies, and then iterate over it and insert in the list where
+        frequency is the index and num is the value.
+        (c) Iterate in reverse throught the freq list and then nest another iteration
+        inside and iterate again to insert each num from each list at every index.
+            (c1) Stop when result len == k
+
+        METHOD 2: Quickselect - Not yet understood.
+        """
+
+        # METHOD 1
+        frequency_map = defaultdict(int)
+        frequency_list = [[] for i in range(len(nums) + 1)]
+        result = []
 
         for num in nums:
-            freq_count[num] += 1
+            frequency_map[num] += 1
+        for num, frequency in frequency_map.items():
+            frequency_list[frequency].append(num)
 
-        for num, freq in freq_count.items():
-            freq_list[freq].append(num)
-
-        result = []
-        for i in range(len(freq_list) - 1, 0, -1):
-            for num in freq_list[i]:
+        for i in range(len(frequency_list) - 1, -1, -1):
+            for num in frequency_list[i]:
                 result.append(num)
                 if len(result) == k:
                     return result
