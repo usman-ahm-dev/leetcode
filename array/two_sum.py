0de345fa9a34662e36f4357f5cdca50e5357a21b
Usman <usman_ahm@icloud.com>
2023-06-04 08:52:01 +0500

    Solved Problem 1

--- array/two_sum.py
+++ array/two_sum.py
@@ -21,10 +21,25 @@ class Solution:
         Returns:
             list[int]:
         """
-        hashmap = {}  # value : index
 
+        """Subtract nums[i] from target (complement) and check if it exists in nums. Use
+        dictionary for O(1) lookups.
+
+        METHOD 1: T = O(N) S = O(N) 
+        (a) Create a dictionary that stores nums[i] : index
+        (b) Iterate through nums
+        (c) Subtract nums[i] from target
+        (d) Check if it exists in dictionary. If it does, return [i, i of complement]
+            IMPORTANT: Same index cannot be used, so check for complement before adding
+            next element (this way i of complement will always be different than i
+            because of the former being i of next value in next iteration).
+            OR do it in 2 passes; Add all indices, then find complement and check if
+            i & i of complement are different.
+        """
+
+        hashmap = {}
         for i, num in enumerate(nums):
-            diff = target - num
-            if diff in hashmap:
-                return [i, hashmap[diff]]
+            complement = target - num
+            if complement in hashmap:
+                return [i, hashmap[complement]]
             hashmap[num] = i
